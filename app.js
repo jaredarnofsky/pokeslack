b84a653135c440f733314df624e33037eefb641a
Jared Arnofsky <jarnofsky@agencyoasis.com>
2016-05-19 10:49:49 -0400

    changed bot include

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var bot = require('./bot');
+var bot = require('bot');
 
 var app = express();
 var port = process.env.PORT || 3000;
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
