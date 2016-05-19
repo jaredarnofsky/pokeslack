330804bb3b95c8c8dd9e9a74237ee7f3bcec6705
Jared Arnofsky <jarnofsky@agencyoasis.com>
2016-05-19 10:21:13 -0400

    cleaned up

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var bot = require('./bot');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
