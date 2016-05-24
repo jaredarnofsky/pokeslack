78eace5dc79219703707aaa0a986c873981af4f4
Jared Arnofsky <jarnofsky@agencyoasis.com>
2016-05-24 11:31:55 -0400

    reverted to original basic functionality

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello, ' + userName + ' - :keith:!'
+    text : 'Hey, ' + userName + '!'
   };
 
   // avoid infinite loop
-  if (userName !== 'projectk') {
+  if (userName !== 'outgoing-webhook') {
     return res.status(200).json(botPayload);
   } else {
     return res.status(200).end();
