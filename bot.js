69be11c718a288f610c548a831947e24476a558c
Jared Arnofsky <jarnofsky@agencyoasis.com>
2016-06-07 14:45:32 -0400

    changed text

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
     //text : 'Hey, ' + userName + '!'
-      text : 'I am the captain now! /gif i am the captain now'
+      text : '/gif i am the captain now'
   };
 
   // avoid infinite loop
