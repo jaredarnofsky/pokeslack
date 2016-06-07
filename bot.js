4c829d0b0fe1f0065036276d761ab9eee1921725
Jared Arnofsky <jarnofsky@agencyoasis.com>
2016-06-07 14:50:12 -0400

    messing around

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ module.exports = function (req, res, next) {
   var botPayload = {
     //text : 'Hey, ' + userName + '!'
       text : '/gif i am the captain now'
+      response_type : "in channel"
   };
 
   // avoid infinite loop
